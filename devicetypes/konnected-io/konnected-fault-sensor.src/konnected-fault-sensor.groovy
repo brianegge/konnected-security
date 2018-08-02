f9b3f9e498f083fcba6b58b57cc930af7e098157
Brian Egge <brianegge@gmail.com>
2018-08-02 18:09:12 -0400

    Add fault sensor for boilers, septic pumps, low water cutoffs, etc..

--- devicetypes/konnected-io/konnected-fault-sensor.src/konnected-fault-sensor.groovy
+++ devicetypes/konnected-io/konnected-fault-sensor.src/konnected-fault-sensor.groovy
@@ -1,7 +1,7 @@
 /**
- *  Konnected Fault Sesnor
+ *  Konnected Fault Sensor
  *
- *  Copyright 2017 konnected.io
+ *  Copyright 2018 Konnected Inc (https://konnected.io)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,24 +14,23 @@
  *
  */
 metadata {
-  // Useful for connecting to a boiler, low water cutoff, high level alarm
   definition (name: "Konnected Fault Sensor", namespace: "konnected-io", author: "konnected.io", mnmn: "SmartThings", vid: "generic-contact") {
-    capability "Switch"
+    capability "Contact Sensor"
     capability "Sensor"
   }
 
   preferences {
     input name: "normalState", type: "enum", title: "Normal State",
-	  options: ["Normally Closed", "Normally Open"],
+      options: ["Normally Closed", "Normally Open"],
       defaultValue: "Normally Open",
-      description: "By default, the alarm state is triggered when the sensor circuit is closed (NO). Select Normally Open (NO) when a closed circuit indicates an alarm."
+      description: "Most boiler alarm dry contacts are Normally Open (NO), meaning that the circuit closes when the boiler is in a fault condition. To reverse this logic, select Normally Closed (NC)."
   }
 
   tiles {
     multiAttributeTile(name:"main", type: "generic", width: 6, height: 4, canChangeIcon: true) {
-      tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-        attributeState ("off", label: "OK",    icon:"st.thermostat.heat",  backgroundColor:"#ffffff")
-        attributeState ("on",  label: "Fault!", icon:"st.thermostat.emergency-heat", backgroundColor:"#e86d13")
+      tileAttribute ("device.contact", key: "PRIMARY_CONTROL") {
+        attributeState ("closed", label: "Fault!", icon:"st.categories.damageAndDanger", backgroundColor:"#e86d13")
+        attributeState ("open",   label: "OK",   icon:"st.contact.contact.open",   backgroundColor:"#ffffff")
       }
     }
     main "main"
@@ -39,18 +38,17 @@ metadata {
   }
 }
 
-
 def isClosed() {
-  normalState == "Normally Closed" ? "on" : "off"
+  normalState == "Normally Closed" ? "open" : "closed"
 }
 
 def isOpen() {
-  normalState == "Normally Closed" ? "off" : "on"
+  normalState == "Normally Closed" ? "closed" : "open"
 }
 
 // Update state sent from parent app
 def setStatus(state) {
   def stateValue = state == "1" ? isOpen() : isClosed()
-  sendEvent(name: "switch", value: stateValue)
+  sendEvent(name: "contact", value: stateValue)
   log.debug "$device.label is $stateValue"
 }
